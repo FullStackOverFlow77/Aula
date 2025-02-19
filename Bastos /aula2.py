319cb4387aa6b18bacc1081bae4bd81921196fd9
FullStackOverFlow77 <NightFace777@gmail.com>
2025-02-19 17:45:47 +0000

    ex

--- Bastos /aula2.py
+++ Bastos /aula2.py
@@ -1,2 +1,8 @@
 # Atividade 2 - Exiba caso a nota seja maior que 7 seja aprovado 
 
+nota = float(input("Digite a nota do aluno: "))
+
+if nota >= 7:
+    print("Aprovado")
+else:
+    print("Reprovado")
